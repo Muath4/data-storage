513b9aabbacb92963dd2c815b77556fc04e838c2
Muath Alshathri <abogbl4@gmail.com>
2023-11-17 14:24:33 +0300

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, PLATFORM_ID, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormControl } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'data-storage';
+  private platformId = inject(PLATFORM_ID)
+  // storedData?: string | null
+  favoriteColorControl = new FormControl();
+  ngOnInit(){
+
+    if (isPlatformBrowser(this.platformId)) {
+      this.favoriteColorControl.setValue(localStorage.getItem('storedData'))
+    } else {
+    }
+  }
+  
+  save(){
+    localStorage.setItem('storedData', this.favoriteColorControl.value || "");
+  }
+
+  remove(){
+    localStorage.removeItem('storedData');
+  }
 }
